d2ac10aa3c349c897bc19095902f8ddc9fb5e8f0
EnderEuropium <36482895+EnderEuropium@users.noreply.github.com>
2023-05-25 15:37:32 +0800

    Update rk_4.m

--- test1/rk_4.m
+++ test1/rk_4.m
@@ -1,19 +1,20 @@
-function [tout,yout]=rk_4(fun,tspan,y0);
+function [tout,yout]=rk_4(fun,tspan,xy0);
+% fun调用格式: 'funfile'
 % tspan=[t0,tn,h]
 ts=tspan;
 t0=ts(1);
 tf=ts(2);
-yout=[];
+xyout=[];
 tout=[];
-y0=y0(:);
+xy0=xy0(:);
 h=ts(3);
 for t= [t0:h:tf] %对各个时间点循环计算
-    k1=h*fun(t,y0);
-    k2=h*fun(t+h/2.y0+k1/2);
-    k3=h*fun(t+h/2,y0+k2/2);
-    k4=h*fun(t+h,y0+k3);
-    y0=y0+(k1+2*k2+2*k3+k4)/6;
-    yout=[yout;y0];
+    k1=h*fun(t,xy0);
+    k2=h*fun(t+h/2.xy0+k1/2);
+    k3=h*fun(t+h/2,xy0+k2/2);
+    k4=h*fun(t+h,xy0+k3);
+    xy0=xy0+(k1+2*k2+2*k3+k4)/6;
+    xyout=[xyout;xy0];
     tout=[tout;t];
 end
 
